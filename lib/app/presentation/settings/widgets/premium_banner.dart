64dac5c8512964b574b3decc6827e25ca09e924b
Osama Abusaada <osamasda111@gmail.com>
2026-09-13 18:41:34 +0300

    Patch(+): removed billing info from app to comply with google play store

--- lib/app/presentation/settings/widgets/premium_banner.dart
+++ lib/app/presentation/settings/widgets/premium_banner.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:uniceps/core/constants/app_routes.dart';
 import 'package:uniceps/core/constants/constants.dart';
 import 'package:uniceps/l10n/app_localizations.dart';
 
 class PremiumBanner extends StatelessWidget {
-  const PremiumBanner({
-    super.key,
-  });
+  const PremiumBanner({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -19,33 +16,27 @@ class PremiumBanner extends StatelessWidget {
           Container(
             padding: const EdgeInsets.all(5.0),
             decoration: const BoxDecoration(shape: BoxShape.circle, color: Colors.amber),
-            child: const Image(
-              image: AssetImage(IMG_PREMIUM),
-              width: 15,
-              height: 15,
-              color: Colors.white,
-            ),
+            child: const Image(image: AssetImage(IMG_PREMIUM), width: 15, height: 15, color: Colors.white),
           ),
           Expanded(
-              child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: Text(locale.premiumAlertContent),
-          )),
-          SizedBox(
-            height: 30,
-            child: OutlinedButton(
-              onPressed: () => Navigator.pushNamed(context, AppRoutes.plans),
-              style: OutlinedButton.styleFrom(
-                padding: const EdgeInsets.symmetric(horizontal: 5.0, vertical: 0.0),
-                foregroundColor: Colors.amber.shade700,
-                side: const BorderSide(color: Colors.amber, width: 1.0),
-              ),
-              child: Text(
-                locale.upgrade,
-                style: const TextStyle(fontSize: 11, fontWeight: FontWeight.bold),
-              ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Text(locale.activationAlertContent),
             ),
           ),
+          // SizedBox(
+          //   height: 30,
+          //   child: OutlinedButton(
+          //     // onPressed: () => Navigator.pushNamed(context, AppRoutes.plans),
+          //     onPressed: null,
+          //     style: OutlinedButton.styleFrom(
+          //       padding: const EdgeInsets.symmetric(horizontal: 5.0, vertical: 0.0),
+          //       foregroundColor: Colors.amber.shade700,
+          //       side: const BorderSide(color: Colors.amber, width: 1.0),
+          //     ),
+          //     child: Text(locale.upgrade, style: const TextStyle(fontSize: 11, fontWeight: FontWeight.bold)),
+          //   ),
+          // ),
         ],
       ),
     );
